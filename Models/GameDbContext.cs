ba967d7cd7ae1828f6ba04bb35792d89167cbd6a
Jason Wu(Aero 15) <wujpkimo@gmail.com>
2019-03-10 18:07:38 +0800

    2019-03-10 Jason Add gitignore

--- Models/GameDbContext.cs
+++ Models/GameDbContext.cs
@@ -4,6 +4,7 @@ namespace EfcoreConsoleApp.Models
 {
     public class GameDbContext : DbContext
     {
+        //DbSet內需放入單數類別，物件則需用複數(s)
         public DbSet<Pokemon> Pokemons { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
